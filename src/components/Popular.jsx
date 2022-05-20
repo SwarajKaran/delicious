84a7e1eaf65e765864317ba68758d3b2a13103c9
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-21 00:08:11 +0530

    Call API and render Popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,58 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 const Popular = () => {
-  return <div>Popular</div>;
+  const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    getPopular();
+  }, []);
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+  };
+  return (
+    <div>
+      <h3>Popular Picks</h3>
+      <Wrapper>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: 'free',
+            gap: '5rem',
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title}></img>
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
 };
+
+const Wrapper = styled.div`
+  margin: 4 rem 0rem;
+`;
+const Card = styled.div`
+  min-height: 25rem;
+  bordee-radius: 2rem;
+  overflow: hidden;
+  img {
+    border-radius: 2rem;
+  }
+`;
 
 export default Popular;
