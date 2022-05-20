fc4e6e7a4985a8c9bdadce02b18abb01c00ac66b
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-21 01:11:53 +0530

    Render Veggie

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -8,11 +8,17 @@ const Popular = () => {
     getPopular();
   }, []);
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
-    const data = await api.json();
-    setPopular(data.recipes);
+    const check = localStorage.getItem('popular');
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+      localStorage.setItem('popular', JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+    }
   };
   return (
     <div>
@@ -33,6 +39,7 @@ const Popular = () => {
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title}></img>
+                  <Gradient />
                 </Card>
               </SplideSlide>
             );
@@ -50,9 +57,39 @@ const Card = styled.div`
   min-height: 25rem;
   bordee-radius: 2rem;
   overflow: hidden;
+  position: relative;
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-alight: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  border-radius: 2rem;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
 
 export default Popular;
