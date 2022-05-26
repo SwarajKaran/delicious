7323746649ff515f05442550bad8da3faa2a84e1
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-26 23:53:01 +0530

    fixing Cuisine

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -38,10 +38,13 @@ const SLink = styled(NavLink)`
   border-radius: 50%;
   margin-right: 2rem;
   text-decoration: none;
+
   background: linear-gradient(35deg, #494949, #313131);
+
   width: 6rem;
   height: 6rem;
   cursor: pointer;
+
   transform: scale(0.8);
 
   h4 {
@@ -51,6 +54,7 @@ const SLink = styled(NavLink)`
   svg {
     color: white;
     font-size: 1.5rem;
+    transform: scale(1.5);
   }
   &.active {
     background: linear-gradient(to right, #f27121, #3e94057);
